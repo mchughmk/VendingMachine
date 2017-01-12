693965c50796253ad8cc20652c14375b0e6bb0e6
Matt McHugh <matt.mchugh@excella.com>
2017-01-12 10:14:17 -0500

    Removing old migration and schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,4 @@ ActiveRecord::Schema.define(version: 20161216211240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
