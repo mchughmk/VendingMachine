cb4e633228d7256f2703b008b7b4b422e0db0260
Andrew <hackedhead@users.noreply.github.com>
2017-01-13 09:33:46 -0500

    Add a Product admin for names and prices (#2)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,6 @@
 class Product < ApplicationRecord
-  attr_reader :name
-  attr_accessor :unit_price
+  #attr_reader :name
+  #attr_accessor :unit_price
   has_many :vending_machine_stocks
   has_many :vending_machines, through: :vending_machine_stocks
-
-  def initialize(name, unit_price)
-    @unit_price = unit_price
-    @name = name
-  end
-
 end
---
+++ app/views/products/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @product, :id, :name, :unit_price, :created_at, :updated_at
